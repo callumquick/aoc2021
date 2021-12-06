902425ffdaeef7e414bd1e85683b6f307d4a7ec7
Callum Ward <wards.callum@gmail.com>
2021-12-06 11:49:48 +0000

    Blazingly quick solution using arrays instead

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,30 +1,30 @@
 /// Solution to Advent of Code Challenge Day 05.
 use aoc2021::{get_day_input, parse_input_with, print_elapsed_time};
-use std::collections::HashMap;
 
 fn calculate_population(starting: &Vec<u32>, cycles: u32) -> u64 {
-    let mut counts: HashMap<u32, u64> = HashMap::with_capacity(8);
+    let mut counts = [0u64; 9];
 
     for fish in starting {
-        *counts.entry(*fish).or_insert(0) += 1;
+        let idx: usize = (*fish).try_into().unwrap();
+        counts[idx] += 1;
     }
 
     for _ in 0..cycles {
-        let start_counts = counts.to_owned();
+        let start_counts = counts;
 
         // Decrease the days left for all fish (that aren't about to reproduce)
         for day in 1..=8 {
-            *counts.entry(day - 1).or_insert(0) = *start_counts.get(&day).unwrap_or(&0);
+            counts[day - 1] = start_counts[day];
         }
 
         // Create new fish at day 8
-        *counts.entry(8).or_insert(0) = *start_counts.get(&0).unwrap_or(&0);
+        counts[8] = start_counts[0];
 
         // Add fish that reproduced to day 6
-        *counts.entry(6).or_insert(0) += start_counts.get(&0).unwrap_or(&0);
+        counts[6] += start_counts[0];
     }
 
-    counts.iter().fold(0, |acc, (_, num)| acc + num)
+    counts.iter().sum()
 }
 
 fn part_one(input: &Vec<u32>) -> u64 {
